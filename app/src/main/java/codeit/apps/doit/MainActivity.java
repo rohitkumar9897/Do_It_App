4ded42fb8c347965081d98aaa92f39dfbe6eb7b6
Rohit Kumar <rohitku8210610477@gmail.com>
2024-01-17 00:11:22 +0530

    Signin and Signup

--- app/src/main/java/codeit/apps/doit/MainActivity.java
+++ app/src/main/java/codeit/apps/doit/MainActivity.java
@@ -2,13 +2,35 @@ package codeit.apps.doit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btn_logout);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent= new Intent(getApplicationContext(), Signin.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
