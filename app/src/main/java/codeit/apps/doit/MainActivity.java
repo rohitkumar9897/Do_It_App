a9ceabe61649e948202d8fe9811857991315aa05
Rohit Kumar <rohitku8210610477@gmail.com>
2024-01-31 01:27:33 +0530

    [ADD]Navigation Drawer

--- app/src/main/java/codeit/apps/doit/MainActivity.java
+++ app/src/main/java/codeit/apps/doit/MainActivity.java
@@ -7,9 +7,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 
 import android.content.Intent;
 import android.os.Bundle;
